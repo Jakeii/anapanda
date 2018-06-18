9f79be3b1abb8ffa74d1b47d38b9e8e493a755e3
unknown <kasenget@gmail.com>
2018-06-18 12:16:04 +0300

    Clean up templates for crispy

--- commitments/urls.py
+++ commitments/urls.py
@@ -21,23 +21,25 @@ from commitments.apps.accounts import views as accounts_views
 from commitments.apps.managetime import views as kuviews
 
 urlpatterns = [
-    path('', kuviews.kummithome, name='home'),
     path('signup/', accounts_views.signup, name='signup'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-    path('kummit/<int:pk>/', kuviews.TaskUpdate.as_view(), name='kummit-view'),
+    
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+
     path('kummit/', kuviews.kummithome, name='kummithome'),
+    path('kummit/<int:pk>/', kuviews.TaskUpdate.as_view(), name='kummit-view'),
     path('kummit/tasks/make/', kuviews.TaskCreate.as_view(), name='task-create'),
     path('kummit/tasks/<int:pk>/', kuviews.TaskView.as_view(), name='task-detail'),
     path('kummit/tasks/<int:pk>/delete/', kuviews.TaskDelete.as_view(), name='kummit-delete'),
     path('kummit/tasks/', kuviews.TaskList.as_view(), name='task-list'),
     path('kummit/kummitments/make/', kuviews.KummitmentCreate.as_view(), name='kummitment-make'),
     path('kummit/kummitments/<int:pk>/', kuviews.KummitmentView.as_view(), name='kummitment-detail'),
-    #path('kummit/kummitments/<int:pk>/delete/', kuviews.KummitmentDelete.as_view(), name='kummitment-delete'),
     path('kummit/kummitments/', kuviews.KummitmentList.as_view(), name='kummitment-list'), 
     path('kummit/circles/<int:pk>/', kuviews.CircleView.as_view(), name='circle-detail'),
     path('kummit/circles/', kuviews.CircleList.as_view(), name='circle-list'),
     path('kummit/roles/<int:pk>/', kuviews.RoleView.as_view(), name='role-detail'),
     path('kummit/roles/', kuviews.RoleList.as_view(), name='role-list'), 
+    
+    path('', kuviews.kummithome, name='home'),
 ]
