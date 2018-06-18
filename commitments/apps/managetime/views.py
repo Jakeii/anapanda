3a6f5f7ead1d758208e0cded77681515fef30c99
unknown <kasenget@gmail.com>
2018-06-18 12:30:49 +0300

    Adds login_required to protect views from authentication

--- commitments/apps/managetime/views.py
+++ commitments/apps/managetime/views.py
@@ -1,19 +1,16 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+
 from django.urls import reverse_lazy
+from django.utils import timezone
+
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic import ListView, DetailView
-from django.utils import timezone
+
 from django_filters.views import FilterView
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+
 from .models import Circle, Role, Task, Kummitment, RoleKummitment, Project
 from .filters import TaskFilter
-# from django.contrib.auth.mixins import LoginRequiredMixin -> to limit access to views https://stackoverflow.com/questions/42481287/automatically-set-logged-in-user-as-the-author-in-django-using-createview-and-mo
-
-#def CircleList(request):
-#    circles = Circle.objects.all()
-#    return render(request, 'kummittemplates/home.html', {'circles': circles})
 
 def kummithome(request):
     return render(request, 'managetime/home.html')
@@ -36,16 +33,12 @@ class CircleList(FilterView):
         context['now'] = timezone.now()
         return context
 
-
-# @method_decorator(login_required, name='dispatch')
 class TaskCreate(CreateView):
     model = Task
     fields = ['subject', 'description', 'doneevidence', 'startdate', 'deadline', 'project', 'role', 'circle']
    
     def get_form(self, form_class=None):
        form = super().get_form(form_class)
-       form.helper = FormHelper()
-       form.helper.add_input(Submit('submit', 'Create', css_class='btn-primary'))
        return form
 
     def form_valid(self, form):
@@ -81,12 +74,21 @@ class TaskList(FilterView):
 
 class KummitmentCreate(CreateView):
     model = Kummitment
-    fields = ['task', 'kummitor', 'successactions', 'nextaction', 'possibleobstacles', 'minutesestimate', 'start', 'deadline', 'energytype', 'weekscheduled', 'dayscheduled', 'timescheduled', 'status', 'specific', 'measurable', 'achievable', 'relevant', 'timebound', 'healthy', 'wealthy', 'wise', 'connected', 'reflection', 'needhelp']
+    fields = [
+        'task', 'kummitor', 
+        'successactions', 'nextaction', 
+        'possibleobstacles', 'minutesestimate', 
+        'start', 'deadline', 'energytype', 
+        'weekscheduled', 'dayscheduled', 
+        'timescheduled', 'status', 'specific', 
+        'measurable', 'achievable', 'relevant', 
+        'timebound', 'healthy', 
+        'wealthy', 'wise', 'connected', 
+        'reflection', 'needhelp'
+    ]
    
     def get_form(self, form_class=None):
        form = super().get_form(form_class)
-       form.helper = FormHelper()
-       form.helper.add_input(Submit('submit', 'Create', css_class='btn-primary'))
        return form
 
     def form_valid(self, form):
@@ -110,7 +112,6 @@ class KummitmentList(FilterView):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
         return context
-
 
 class RoleView(DetailView):
     model = Role
--- commitments/urls.py
+++ commitments/urls.py
@@ -13,33 +13,38 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+
+from django.contrib import admin
 from django.contrib.auth import views as auth_views
+#Putting login required here for now
+from django.contrib.auth.decorators import login_required
 
 from commitments.apps.accounts import views as accounts_views
 from commitments.apps.managetime import views as kuviews
 
-urlpatterns = [
-    path('signup/', accounts_views.signup, name='signup'),
-    path('admin/', admin.site.urls),
-    
+urlpatterns = [   
+    path('accounts/signup/', accounts_views.signup, name='signup'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    path('kummit/', kuviews.kummithome, name='kummithome'),
-    path('kummit/<int:pk>/', kuviews.TaskUpdate.as_view(), name='kummit-view'),
-    path('kummit/tasks/make/', kuviews.TaskCreate.as_view(), name='task-create'),
-    path('kummit/tasks/<int:pk>/', kuviews.TaskView.as_view(), name='task-detail'),
-    path('kummit/tasks/<int:pk>/delete/', kuviews.TaskDelete.as_view(), name='kummit-delete'),
-    path('kummit/tasks/', kuviews.TaskList.as_view(), name='task-list'),
-    path('kummit/kummitments/make/', kuviews.KummitmentCreate.as_view(), name='kummitment-make'),
-    path('kummit/kummitments/<int:pk>/', kuviews.KummitmentView.as_view(), name='kummitment-detail'),
-    path('kummit/kummitments/', kuviews.KummitmentList.as_view(), name='kummitment-list'), 
-    path('kummit/circles/<int:pk>/', kuviews.CircleView.as_view(), name='circle-detail'),
-    path('kummit/circles/', kuviews.CircleList.as_view(), name='circle-list'),
-    path('kummit/roles/<int:pk>/', kuviews.RoleView.as_view(), name='role-detail'),
-    path('kummit/roles/', kuviews.RoleList.as_view(), name='role-list'), 
+    path('kummit/', login_required(kuviews.kummithome), name='kummithome'),
+    path('kummit/<int:pk>/', login_required(kuviews.TaskUpdate.as_view()), name='kummit-view'),
+    path('kummit/tasks/make/', login_required(kuviews.TaskCreate.as_view()), name='task-create'),
+    path('kummit/tasks/<int:pk>/', login_required(kuviews.TaskView.as_view()), name='task-detail'),
+    path('kummit/tasks/<int:pk>/delete/', login_required(kuviews.TaskDelete.as_view()), name='kummit-delete'),
+    path('kummit/tasks/', login_required(kuviews.TaskList.as_view()), name='task-list'),
+    path('kummit/kummitments/make/', login_required(kuviews.KummitmentCreate.as_view()), name='kummitment-make'),
+    path('kummit/kummitments/<int:pk>/', login_required(kuviews.KummitmentView.as_view()), name='kummitment-detail'),
+    path('kummit/kummitments/', login_required(kuviews.KummitmentList.as_view()), name='kummitment-list'), 
+    path('kummit/circles/<int:pk>/', login_required(kuviews.CircleView.as_view()), name='circle-detail'),
+    path('kummit/circles/', login_required(kuviews.CircleList.as_view()), name='circle-list'),
+    path('kummit/roles/<int:pk>/', login_required(kuviews.RoleView.as_view()), name='role-detail'),
+    path('kummit/roles/', login_required(kuviews.RoleList.as_view()), name='role-list'), 
     
-    path('', kuviews.kummithome, name='home'),
+    path('', login_required(kuviews.kummithome), name='home'),
+]
+
+urlpatterns += [
+    path('admin/', admin.site.urls),
 ]
