876979278c9df48884f8001f80265f8f476ae198
Jake Lee Kennedy <jake@bodhi.io>
2018-09-07 16:37:11 +0100

    add users REST endpoint

--- commitments/apps/rest_api/serializers.py
+++ commitments/apps/rest_api/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import routers, serializers, viewsets
 from commitments.apps.managetime.models import Task
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    created_tasks = serializers.PrimaryKeyRelatedField(many=True, queryset=Task.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'created_tasks')
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('subject', 'description', 'done_evidence', 'start_date')
+        fields = ('id', 'subject', 'description', 'done_evidence', 'start_date', 'created_by')
