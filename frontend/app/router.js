a2777fc5666d6854c968af3026fc8b59abbfa710
Jake Lee Kennedy <jake@bodhi.io>
2018-09-07 16:38:07 +0100

    start implementation of ember frontend

---
+++ frontend/app/router.js
@@ -0,0 +1,21 @@
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
+
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('login');
+  this.route('signup');
+  this.route('tasks', { path: '/tasks' }, function() {
+    this.route('make');
+    this.route('single', { path: '/:id' })
+  });
+  this.route('circles');
+  this.route('roles');
+  this.route('kummitments');
+});
+
+export default Router;
