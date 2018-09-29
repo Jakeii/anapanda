c5127cafb524c7e3bfd7e4f982253cbce65a5d59
Jake Lee Kennedy <jake@bodhi.io>
2018-09-29 19:31:41 +0100

    fix model import syntax

--- frontend/app/models/project.js
+++ frontend/app/models/project.js
@@ -1,4 +1,6 @@
-import { Model, attr, belongsTo, hasMany } from 'ember-data';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   name: attr(),
