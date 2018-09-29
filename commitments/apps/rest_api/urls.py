ff4561ca6184c464e649da6cfa1e2cefd53d10b5
Jake Lee Kennedy <jake@bodhi.io>
2018-09-29 18:28:45 +0100

    flesh out views, serializers and urls for remaining models

--- commitments/apps/rest_api/urls.py
+++ commitments/apps/rest_api/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import url, include
+""" url routes for the REST API """
+from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_jwt.views import obtain_jwt_token
 from . import views
 
 
@@ -10,8 +12,21 @@ urlpatterns = [
 
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+
     url(r'^tasks/$', views.TaskList.as_view()),
-    url(r'^tasks/(?P<pk>[0-9]+)/$', views.TaskDetail.as_view())
+    url(r'^tasks/(?P<pk>[0-9]+)/$', views.TaskDetail.as_view()),
+
+    url(r'^kummitments/$', views.KummitmentList.as_view()),
+    url(r'^kummitments/(?P<pk>[0-9]+)/$', views.KummitmentDetail.as_view()),
+
+    url(r'^projects/$', views.ProjectList.as_view()),
+    url(r'^projects/(?P<pk>[0-9]+)/$', views.ProjectDetail.as_view()),
+
+    url(r'^roles/$', views.RoleList.as_view()),
+    url(r'^roles/(?P<pk>[0-9]+)/$', views.RoleDetail.as_view()),
+
+    url(r'^circles/$', views.CircleList.as_view()),
+    url(r'^circles/(?P<pk>[0-9]+)/$', views.CircleDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- commitments/apps/rest_api/views.py
+++ commitments/apps/rest_api/views.py
@@ -1,5 +1,5 @@
-from .serializers import TaskSerializer, UserSerializer
-from commitments.apps.managetime.models import Task
+from .serializers import TaskSerializer, UserSerializer, KummitmentSerializer, ProjectSerializer, RoleSerializer, CircleSerializer
+from commitments.apps.managetime.models import Task, Kummitment, Project, Role, Circle
 from django.contrib.auth.models import User
 from rest_framework import generics
 
@@ -19,3 +19,39 @@ class TaskList(generics.ListCreateAPIView):
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+
+class KummitmentList(generics.ListCreateAPIView):
+    queryset = Kummitment.objects.all()
+    serializer_class = KummitmentSerializer
+
+class KummitmentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Kummitment.objects.all()
+    serializer_class = KummitmentSerializer
+
+
+class ProjectList(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class RoleList(generics.ListCreateAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+class RoleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+
+class CircleList(generics.ListCreateAPIView):
+    queryset = Circle.objects.all()
+    serializer_class = CircleSerializer
+
+class CircleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Circle.objects.all()
+    serializer_class = CircleSerializer
