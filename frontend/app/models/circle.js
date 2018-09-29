b7076b157c56d28ae7a51159cbb61f3f0deacaf8
Jake Lee Kennedy <jake@bodhi.io>
2018-09-29 21:55:10 +0100

    extend model for circle

--- frontend/app/models/circle.js
+++ frontend/app/models/circle.js
@@ -2,7 +2,7 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 
-export default Model({
+export default Model.extend({
   name: attr(),
   purpose: attr(),
 
